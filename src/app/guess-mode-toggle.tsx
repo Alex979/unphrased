d331d904eb70ddef2d5d57add3d668f75b7ab740
Alex Broaddus <alexbroaddus1@gmail.com>
2024-04-08 14:40:22 -0700

    Fixing incorrect focus rect on toggle button

--- src/app/guess-mode-toggle.tsx
+++ src/app/guess-mode-toggle.tsx
@@ -34,31 +34,33 @@ export default function GuessModeToggle({
 
   return (
     <button
-      className={`my-2 ${className}`}
       onClick={handleClick}
       onKeyDown={handleKeyDown}
+      className="rounded-full"
     >
-      <div
-        className={`w-1/2 inline ${
-          finalGuess ? "hidden" : ""
-        } px-6 py-2 rounded-l-full border-t border-b border-l ${
-          guessingMode === GuessingMode.Individual
-            ? "border-r border-indigo-600 dark:border-indigo-400 bg-indigo-50 dark:bg-indigo-950 font-semibold text-indigo-600 dark:text-indigo-400"
-            : "border-gray-400 dark:border-zinc-400 text-gray-600 dark:text-zinc-300"
-        }`}
-      >
-        {finalGuess ? "" : "Guess letters"}
-      </div>
-      <div
-        className={`w-1/2 inline px-6 py-2 rounded-r-full ${
-          finalGuess ? "rounded-l-full" : ""
-        } border-t border-b border-r ${
-          guessingMode === GuessingMode.Full
-            ? "border-l border-pink-600 dark:border-pink-300 bg-pink-50 dark:bg-pink-950 font-semibold text-pink-600 dark:text-pink-300"
-            : "border-gray-300 dark:border-zinc-400 text-gray-600 dark:text-zinc-300"
-        }`}
-      >
-        {finalGuess ? "Final Guess!" : "Guess phrase"}
+      <div className={`my-2 ${className}`}>
+        <div
+          className={`w-1/2 inline ${
+            finalGuess ? "hidden" : ""
+          } px-6 py-2 rounded-l-full border-t border-b border-l ${
+            guessingMode === GuessingMode.Individual
+              ? "border-r border-indigo-600 dark:border-indigo-400 bg-indigo-50 dark:bg-indigo-950 font-semibold text-indigo-600 dark:text-indigo-400"
+              : "border-gray-400 dark:border-zinc-400 text-gray-600 dark:text-zinc-300"
+          }`}
+        >
+          {finalGuess ? "" : "Guess letters"}
+        </div>
+        <div
+          className={`w-1/2 inline px-6 py-2 rounded-r-full ${
+            finalGuess ? "rounded-l-full" : ""
+          } border-t border-b border-r ${
+            guessingMode === GuessingMode.Full
+              ? "border-l border-pink-600 dark:border-pink-300 bg-pink-50 dark:bg-pink-950 font-semibold text-pink-600 dark:text-pink-300"
+              : "border-gray-300 dark:border-zinc-400 text-gray-600 dark:text-zinc-300"
+          }`}
+        >
+          {finalGuess ? "Final Guess!" : "Guess phrase"}
+        </div>
       </div>
     </button>
   );
