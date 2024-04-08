ee41bd723054ca646a45f63fa5b5c77701d9d6e1
Alex Broaddus <alexbroaddus1@gmail.com>
2024-04-08 00:24:32 -0700

    Changing header logo

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,8 +4,8 @@ const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
 
 export default function Header() {
   return (
-    <header className="w-full p-4 border-b">
-      <h1 className={`text-3xl font-black text-center ${robotoSlab.className}`}>Unphrased</h1>
+    <header className="w-full py-2 px-6 border-b">
+      <h1 className={`text-3xl font-black tracking-tight underline ${robotoSlab.className}`}>Unphrased</h1>
     </header>
   );
 }
