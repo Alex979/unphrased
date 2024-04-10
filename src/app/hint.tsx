351b4e5a2699e21ddace2648304a6344a5d8dd40
Alex Broaddus <alexbroaddus1@gmail.com>
2024-04-09 17:51:23 -0700

    Removing border in colored part of emoji hint

--- src/app/hint.tsx
+++ src/app/hint.tsx
@@ -8,13 +8,13 @@ interface HintProps {
 export default function Hint({ emojis, guessingMode }: HintProps) {
   const colorClass =
     guessingMode === GuessingMode.Individual
-      ? `bg-indigo-100 dark:bg-indigo-950 border-indigo-300 dark:border-indigo-600`
-      : `bg-pink-100 dark:bg-pink-950 dark:bg-opacity-30 border-pink-300 dark:border-[#a64c74]`;
+      ? `bg-indigo-200 dark:bg-indigo-900`
+      : `bg-pink-200 dark:bg-pink-900`;
 
   return (
     <div className="rounded-lg m-4">
       <div
-        className={`${colorClass} border-x border-t rounded-t-lg text-center`}
+        className={`${colorClass} rounded-t-lg text-center`}
       >
         <p className="text-sm font-bold">CLUE</p>
       </div>
