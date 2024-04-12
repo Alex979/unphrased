b16b216ce23b27ca1a8b0c032511b5a48185cc05
Alex Broaddus <alexbroaddus1@gmail.com>
2024-04-12 10:43:38 -0700

    Changing metadata and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,12 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Unphrased",
-  description: "Reveal the hidden phrase in 8 guesses. New phrase each day.",
+  title: "Unphrased - Daily phrase guessing game",
+  description: "Reveal the hidden phrase in 8 guesses. A new phrase every day.",
   openGraph: {
     title: "Unphrased - A daily phrase guessing game",
     images: "https://www.unphrased.app/unphrased_og.png",
+    siteName: "Unphrased",
   },
 };
 
