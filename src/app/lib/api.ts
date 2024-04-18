91c12949a30345e6a378bc75580a574afacec755
Alex Broaddus <alexbroaddus1@gmail.com>
2024-04-17 18:10:23 -0700

    Adding a custom GA event for finishing the puzzle

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,10 +1,18 @@
 import { getFingerprint } from "@thumbmarkjs/thumbmarkjs";
+import { sendGAEvent } from "@next/third-parties/google";
 
 export function logStatsToServer(
   puzzleId: string,
   solved: boolean,
   numGuesses?: number
 ) {
+  sendGAEvent({
+    event: 'puzzle_finished',
+    puzzle_id: puzzleId,
+    solved,
+    num_guesses: numGuesses,
+  });
+  
   getFingerprint().then((fingerprint) => {
     fetch("/api/log-stats", {
       method: "POST",
