8a49491f9e4c7f1051428fc8971baf47451a922f
Alex Broaddus <alexbroaddus1@gmail.com>
2024-05-15 21:01:16 -0700

    Styling changes to finish screen

--- src/app/finish-screen.tsx
+++ src/app/finish-screen.tsx
@@ -22,29 +22,29 @@ export default function FinishScreen({
   maxGuesses,
 }: FinishScreenProps) {
   return (
-    <div className="w-full h-full flex justify-center pt-10">
-      <div className="p-8">
+    <div className="w-full h-full flex justify-center">
+      <div className="px-8">
         <h1
-          className={`font-extrabold text-4xl mb-6 leading-tight ${robotoSlab.className}`}
+          className={`text-center font-extrabold text-3xl mb-4 leading-tight ${robotoSlab.className}`}
         >
           {didWin ? "Well done!" : "Better luck next time!"}
         </h1>
         {didWin ? (
-          <p className="my-2 text-lg">
+          <p className="my-2 text-center">
             You solved the phrase in{" "}
             <span className="font-bold">{guessCount}</span>{" "}
-            {guessCount === 1 ? "guess" : "guesses"}!
+            {guessCount === 1 ? "guess" : "guesses"}.
           </p>
         ) : (
-          <p className="my-2 text-lg">The phrase was:</p>
+          <p className="my-2 text-center">The phrase was</p>
         )}
         <BlockQuote>{sentence.toUpperCase()}</BlockQuote>
-        <p className="my-2 text-lg">
+        <p className="my-2 text-center">
           Come back tomorrow to guess a new phrase.
         </p>
         <div className="flex justify-center">
           <ShareButton
-            className="my-8"
+            className="my-2"
             guessHistory={guessHistory}
             maxGuesses={maxGuesses}
             puzzleNumber={puzzleNumber}
