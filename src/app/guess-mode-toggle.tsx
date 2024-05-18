51ea8ce7989584191a3e4c6aaa88215f26abfb78
Alex Broaddus <alexbroaddus1@gmail.com>
2024-05-18 16:17:23 -0700

    Minor changes to legacy guess mode toggle

--- src/app/guess-mode-toggle.tsx
+++ src/app/guess-mode-toggle.tsx
@@ -31,16 +31,6 @@ export default function GuessModeToggle({
     }
   };
 
-  const pillPositionStyles =
-    guessingMode === GuessingMode.Individual
-      ? "animate-switch-letters"
-      : "animate-switch-phrase";
-
-  const pillColorStyles =
-    guessingMode === GuessingMode.Individual
-      ? "border-2 border-indigo-500 bg-indigo-200 dark:bg-indigo-900"
-      : "border-2 border-pink-500 bg-pink-200 dark:bg-pink-900";
-
   return (
     <button
       onClick={handleClick}
@@ -51,20 +41,20 @@ export default function GuessModeToggle({
         <div
           className={`w-1/2 flex items-center justify-center px-6 py-1.5 rounded-l-full border-t border-b border-l ${
             guessingMode === GuessingMode.Individual
-              ? "border-r border-indigo-600 dark:border-indigo-400 bg-indigo-50 dark:bg-indigo-950 font-semibold text-indigo-600 dark:text-indigo-400"
+              ? "border-r border-indigo-600 dark:border-indigo-400 bg-indigo-50 dark:bg-indigo-950 font-medium text-indigo-600 dark:text-indigo-300 dark:bg-opacity-50"
               : "border-gray-400 dark:border-zinc-400 text-gray-600 dark:text-zinc-300"
           } ${finalGuess ? "opacity-50" : ""}`}
         >
-          {"Guess letters"}
+          Guess letters
         </div>
         <div
           className={`w-1/2 px-6 py-1.5 rounded-r-full border-t border-b border-r ${
             guessingMode === GuessingMode.Full
-              ? "border-l border-pink-600 dark:border-pink-300 bg-pink-50 dark:bg-pink-950 font-semibold text-pink-600 dark:text-pink-300"
-              : "border-gray-300 dark:border-zinc-400 text-gray-600 dark:text-zinc-300"
+              ? "border-l border-pink-600 dark:border-pink-400 bg-pink-50 dark:bg-pink-950 font-medium text-pink-600 dark:text-pink-300 dark:bg-opacity-50"
+              : "border-gray-400 dark:border-zinc-400 text-gray-600 dark:text-zinc-300"
           }`}
         >
-          {"Guess phrase"}
+          Guess phrase
         </div>
       </div>
     </button>
