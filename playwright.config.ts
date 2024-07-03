4ae8d338585321816c0f46783a02bb04cbdc1c11
Alex Broaddus <alexbroaddus1@gmail.com>
2024-07-03 13:00:14 -0700

    Adding build command to playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -67,8 +67,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run start",
+    command: "npm run build && npm run start",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000, // Allow 2 minutes
   },
 });
