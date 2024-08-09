cc34c2000a3cc2054c8591763a745d2bf4129475
Alex Broaddus <alexbroaddus1@gmail.com>
2024-08-08 18:13:38 -0700

    Setting up capacitor integration

--- src/app/puzzle/[id]/page.tsx
+++ src/app/puzzle/[id]/page.tsx
@@ -1,5 +1,23 @@
 import Home from "@/app/page";
+import { Database } from "@/app/supabase/types";
+import { createClient } from "@supabase/supabase-js";
 
 export default function PuzzleArchive({ params }: { params: { id: string } }) {
   return <Home requestedId={params.id} />;
+}
+
+export async function generateStaticParams() {
+  const supabaseAdmin = createClient<Database>(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+
+  const { data, error } = await supabaseAdmin.from("puzzles").select();
+  if (error) {
+    throw error;
+  }
+
+  return data.map((puzzle) => ({
+    id: puzzle.id,
+  }));
 }
