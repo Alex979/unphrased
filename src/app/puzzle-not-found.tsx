57b8ec0effe79f633f27476787bf14b71612d880
Alex Broaddus <alexbroaddus1@gmail.com>
2024-10-06 11:06:09 -0700

    Adding link to archive on puzzle not found page

--- src/app/puzzle-not-found.tsx
+++ src/app/puzzle-not-found.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import HeaderTemplate from "./header-template";
 import { Roboto_Slab } from "next/font/google";
 
@@ -6,12 +7,21 @@ const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
 export default function PuzzleNotFound() {
   return (
     <HeaderTemplate>
-      <div className="text-center pt-40">
+      <div className="text-center pt-40 px-12">
         <h1 className={`text-2xl font-extrabold ${robotoSlab.className} mb-4`}>
           No puzzle right now!
         </h1>
         <p className="text-xl mb-8">I forgor 💀</p>
-        <p>Please check again later.</p>
+        <p className="leading-relaxed">
+          Please check again later, or visit the{" "}
+          <Link
+            href="/archive"
+            className="underline text-indigo-500 hover:text-indigo-400 dark:text-indigo-400 dark:hover:text-indigo-300"
+          >
+            archive
+          </Link>{" "}
+          to play previous puzzles.
+        </p>
       </div>
     </HeaderTemplate>
   );
